67caed601e31dbde654629b108f5ec1f056f7809
Andy Pymont <andy@andypymont.co.uk>
2024-12-01 09:37:15 +0000

    Day 1 Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeMap;
 use std::str::FromStr;
 
 advent_of_code::solution!(1);
@@ -9,6 +10,11 @@ struct LocationList {
 }
 
 impl LocationList {
+    fn sort(&mut self) {
+        self.left.sort_unstable();
+        self.right.sort_unstable();
+    }
+
     fn total_distance(&self) -> u32 {
         self.left
             .iter()
@@ -17,6 +23,25 @@ impl LocationList {
                 Some(r) => l.abs_diff(*r),
                 None => 0,
             })
+            .sum()
+    }
+
+    fn right_counts(&self) -> BTreeMap<u32, u32> {
+        let mut counts = BTreeMap::new();
+
+        for item in &self.right {
+            let count = 1 + counts.get(item).unwrap_or(&0);
+            counts.insert(*item, count);
+        }
+
+        counts
+    }
+
+    fn similarity_score(&self) -> u32 {
+        let right_counts = self.right_counts();
+        self.left
+            .iter()
+            .map(|l| l * right_counts.get(l).unwrap_or(&0))
             .sum()
     }
 }
@@ -51,9 +76,6 @@ impl FromStr for LocationList {
             right.push(r);
         }
 
-        left.sort_unstable();
-        right.sort_unstable();
-
         Ok(LocationList { left, right })
     }
 }
@@ -61,29 +83,50 @@ impl FromStr for LocationList {
 #[must_use]
 pub fn part_one(input: &str) -> Option<u32> {
     match input.parse::<LocationList>() {
-        Ok(list) => Some(list.total_distance()),
+        Ok(mut list) => {
+            list.sort();
+            Some(list.total_distance())
+        }
         Err(_) => None,
     }
 }
 
 #[must_use]
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    match input.parse::<LocationList>() {
+        Ok(list) => Some(list.similarity_score()),
+        Err(_) => None,
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn example_list() -> LocationList {
+        LocationList {
+            left: vec![3, 4, 2, 1, 3, 3],
+            right: vec![4, 3, 5, 3, 9, 3],
+        }
+    }
+
     #[test]
     fn test_parse_input() {
         assert_eq!(
             advent_of_code::template::read_file("examples", DAY).parse(),
-            Ok(LocationList {
-                left: vec![1, 2, 3, 3, 3, 4,],
-                right: vec![3, 3, 3, 4, 5, 9,],
-            })
+            Ok(example_list())
         );
+    }
+
+    #[test]
+    fn test_right_counts() {
+        let mut expected = BTreeMap::new();
+        expected.insert(3, 3);
+        expected.insert(4, 1);
+        expected.insert(5, 1);
+        expected.insert(9, 1);
+
+        assert_eq!(example_list().right_counts(), expected);
     }
 
     #[test]
@@ -95,6 +138,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
