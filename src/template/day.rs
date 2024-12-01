a9533fb08185f7d20a9c410b1b849079cd7e913d
Andy Pymont <andy@andypymont.co.uk>
2024-12-01 00:43:05 +0000

    Add missing #[must_use] notes to library functions to pass pedantic clippy

--- src/template/day.rs
+++ src/template/day.rs
@@ -24,6 +24,7 @@ pub struct Day(u8);
 impl Day {
     /// Creates a [`Day`] from the provided value if it's in the valid range,
     /// returns [`None`] otherwise.
+    #[must_use]
     pub fn new(day: u8) -> Option<Self> {
         if day == 0 || day > 25 {
             return None;
@@ -33,11 +34,13 @@ impl Day {
 
     // Not part of the public API
     #[doc(hidden)]
+    #[must_use]
     pub const fn __new_unchecked(day: u8) -> Self {
         Self(day)
     }
 
     /// Converts the [`Day`] into an [`u8`].
+    #[must_use]
     pub fn into_inner(self) -> u8 {
         self.0
     }
@@ -101,6 +104,7 @@ impl Display for DayFromStrError {
 /* -------------------------------------------------------------------------- */
 
 /// An iterator that yields every day of advent from the 1st to the 25th.
+#[must_use]
 pub fn all_days() -> AllDays {
     AllDays::new()
 }
@@ -112,6 +116,7 @@ pub struct AllDays {
 
 impl AllDays {
     #[allow(clippy::new_without_default)]
+    #[must_use]
     pub fn new() -> Self {
         Self { current: 1 }
     }
