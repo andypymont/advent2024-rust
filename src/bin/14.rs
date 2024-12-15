e6644870ddf9a4b1944551724f22f8491e4e10c9
Andy Pymont <andy@andypymont.co.uk>
2024-12-15 11:32:45 +0000

    Day 14 Part 2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -49,6 +49,53 @@ fn robots_in_quadrants_after(
     (top_left, top_right, bottom_left, bottom_right)
 }
 
+fn find_drawing(robots: &[Robot], width: i32, height: i32) -> i32 {
+    let mut min_x = None;
+    let mut min_y = None;
+
+    for seconds in 1..width.max(height) {
+        let positions: Vec<Point> = robots
+            .iter()
+            .map(|robot| robot.position_after(seconds, width, height))
+            .collect();
+
+        let mut x_variance = 0;
+        let mut y_variance = 0;
+        for i in 0..positions.len() {
+            for j in i..positions.len() {
+                x_variance += positions[i].0.abs_diff(positions[j].0);
+                y_variance += positions[i].1.abs_diff(positions[j].1);
+            }
+        }
+        if seconds <= width {
+            min_x = min_x.map_or(Some((seconds, x_variance)), |(secs, var)| {
+                if var <= x_variance {
+                    Some((secs, var))
+                } else {
+                    Some((seconds, x_variance))
+                }
+            });
+        }
+        if seconds <= height {
+            min_y = min_y.map_or(Some((seconds, y_variance)), |(secs, var)| {
+                if var <= y_variance {
+                    Some((secs, var))
+                } else {
+                    Some((seconds, y_variance))
+                }
+            });
+        }
+    }
+
+    let (x_rem, _) = min_x.unwrap_or((0, 0));
+    let (y_rem, _) = min_y.unwrap_or((0, 0));
+    let mut time = x_rem;
+    while time.rem_euclid(height) != y_rem {
+        time += width;
+    }
+    time
+}
+
 #[derive(Debug, PartialEq)]
 struct ParseRobotError;
 
@@ -91,8 +138,8 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 #[allow(clippy::missing_const_for_fn)]
 #[must_use]
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    parse_robots(input).map_or(None, |robots| Some(find_drawing(&robots, 101, 103)))
 }
 
 #[cfg(test)]
@@ -101,6 +148,82 @@ mod tests {
 
     fn example_robots() -> Vec<Robot> {
         vec![
+            Robot {
+                position: (0, 6),
+                velocity: (1, 1),
+            },
+            Robot {
+                position: (6, 9),
+                velocity: (-1, 2),
+            },
+            Robot {
+                position: (8, 6),
+                velocity: (-1, 1),
+            },
+            Robot {
+                position: (2, 4),
+                velocity: (6, -4),
+            },
+            Robot {
+                position: (0, 6),
+                velocity: (2, 2),
+            },
+            Robot {
+                position: (8, 3),
+                velocity: (4, -7),
+            },
+            Robot {
+                position: (0, 0),
+                velocity: (1, -1),
+            },
+            Robot {
+                position: (1, 4),
+                velocity: (12, 0),
+            },
+            Robot {
+                position: (8, 0),
+                velocity: (-1, -1),
+            },
+        ]
+    }
+
+    #[test]
+    fn test_parse_robot() {
+        assert_eq!(
+            "p=0,4 v=3,-3".parse(),
+            Ok(Robot {
+                position: (0, 4),
+                velocity: (3, -3)
+            })
+        );
+        assert_eq!(
+            "p=6,3 v=-1,-3".parse(),
+            Ok(Robot {
+                position: (6, 3),
+                velocity: (-1, -3)
+            })
+        );
+        assert_eq!(
+            "p=7,3 v=-1,2".parse(),
+            Ok(Robot {
+                position: (7, 3),
+                velocity: (-1, 2)
+            })
+        );
+    }
+
+    #[test]
+    fn test_robot_position_after() {
+        let robot = Robot {
+            position: (2, 4),
+            velocity: (2, -3),
+        };
+        assert_eq!(robot.position_after(5, 11, 7), (1, 3));
+    }
+
+    #[test]
+    fn test_robots_in_quadrants_after() {
+        let robots = vec![
             Robot {
                 position: (0, 4),
                 velocity: (3, -3),
@@ -149,61 +272,18 @@ mod tests {
                 position: (9, 5),
                 velocity: (-3, -3),
             },
-        ]
-    }
-
-    #[test]
-    fn test_parse_robot() {
-        let robots = example_robots();
-        assert_eq!(
-            "p=0,4 v=3,-3".parse(),
-            Ok(Robot {
-                position: (0, 4),
-                velocity: (3, -3)
-            })
-        );
-        assert_eq!(
-            "p=6,3 v=-1,-3".parse(),
-            Ok(Robot {
-                position: (6, 3),
-                velocity: (-1, -3)
-            })
-        );
-        assert_eq!(
-            "p=7,3 v=-1,2".parse(),
-            Ok(Robot {
-                position: (7, 3),
-                velocity: (-1, 2)
-            })
-        );
-    }
-
-    #[test]
-    fn test_parse_robots() {
-        assert_eq!(
-            parse_robots(&advent_of_code::template::read_file("examples", DAY)),
-            Ok(example_robots())
-        );
-    }
-
-    #[test]
-    fn test_robot_position_after() {
-        let robot = Robot {
-            position: (2, 4),
-            velocity: (2, -3),
-        };
-        assert_eq!(robot.position_after(5, 11, 7), (1, 3));
-    }
-
-    #[test]
-    fn test_robots_in_quadrants_after() {
-        let robots = example_robots();
+        ];
         assert_eq!(robots_in_quadrants_after(&robots, 100, 11, 7), (1, 3, 4, 1),);
+    }
+
+    #[test]
+    fn test_find_drawing() {
+        assert_eq!(find_drawing(&example_robots(), 11, 7), 46);
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(5252));
     }
 }
