d29ce2e78517ca165c4df4d100edde36d4fff70d
Andy Pymont <andy@andypymont.co.uk>
2024-12-22 16:24:41 +0000

    Day 22 Part 1: Rewrite with iterator ready for part 2

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,28 +1,62 @@
+use std::iter::successors;
+use std::str::FromStr;
+
 advent_of_code::solution!(22);
 
 const MIX: u64 = 16_777_216;
 
-fn secret_number(initial: u64, number: u64) -> u64 {
-    let mut secret = initial;
-    for _ in 0..number {
-        let mult = secret * 64;
-        secret = (secret ^ mult) % MIX;
-        let div = secret / 32;
-        secret = (secret ^ div) % MIX;
-        let mult2 = secret * 2048;
-        secret = (secret ^ mult2) % MIX;
+#[derive(Debug, PartialEq)]
+struct Buyer {
+    secret: u64,
+}
+
+impl Buyer {
+    const fn next_secret_number(secret: u64) -> u64 {
+        let secret = (secret ^ (secret * 64)) % MIX;
+        let secret = (secret ^ (secret / 32)) % MIX;
+        (secret ^ (secret * 2048)) % MIX
     }
-    secret
+
+    fn secret_numbers(&self) -> impl Iterator<Item = u64> {
+        successors(Some(self.secret), |n| Some(Self::next_secret_number(*n))).take(2001)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Market {
+    buyers: Vec<Buyer>,
+}
+
+impl Market {
+    fn total_final_secret_numbers(&self) -> u64 {
+        self.buyers
+            .iter()
+            .map(|buyer| buyer.secret_numbers().last().unwrap_or(0))
+            .sum()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct ParseMarketError;
+
+impl FromStr for Market {
+    type Err = ParseMarketError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut buyers = Vec::new();
+
+        for line in input.lines() {
+            let secret = line.parse().map_err(|_| ParseMarketError)?;
+            buyers.push(Buyer { secret });
+        }
+
+        Ok(Self { buyers })
+    }
 }
 
 #[must_use]
 pub fn part_one(input: &str) -> Option<u64> {
-    Some(
-        input
-            .lines()
-            .filter_map(|line| line.parse().map_or(None, |x| Some(secret_number(x, 2000))))
-            .sum(),
-    )
+    Market::from_str(input).map_or(None, |market| Some(market.total_final_secret_numbers()))
 }
 
 #[allow(clippy::missing_const_for_fn)]
@@ -35,13 +69,45 @@ pub fn part_two(_input: &str) -> Option<u32> {
 mod tests {
     use super::*;
 
+    fn example_market() -> Market {
+        Market {
+            buyers: vec![
+                Buyer { secret: 1 },
+                Buyer { secret: 10 },
+                Buyer { secret: 100 },
+                Buyer { secret: 2024 },
+            ],
+        }
+    }
+
     #[test]
-    fn test_secret_number() {
-        assert_eq!(secret_number(123, 1), 15887950);
-        assert_eq!(secret_number(123, 2), 16495136);
-        assert_eq!(secret_number(123, 3), 527345);
-        assert_eq!(secret_number(1, 2000), 8685429);
-        assert_eq!(secret_number(10, 2000), 4700978);
+    fn test_parse_input() {
+        assert_eq!(
+            Market::from_str(&advent_of_code::template::read_file("examples", DAY)),
+            Ok(example_market()),
+        );
+    }
+
+    #[test]
+    fn test_secret_numbers() {
+        let buyer = Buyer { secret: 123 };
+        let mut secrets = buyer.secret_numbers();
+        assert_eq!(secrets.next(), Some(123));
+        assert_eq!(secrets.next(), Some(15887950));
+        assert_eq!(secrets.next(), Some(16495136));
+        assert_eq!(secrets.next(), Some(527345));
+
+        let buyer = Buyer { secret: 1 };
+        assert_eq!(buyer.secret_numbers().last(), Some(8685429));
+
+        let buyer = Buyer { secret: 10 };
+        assert_eq!(buyer.secret_numbers().last(), Some(4700978));
+
+        let buyer = Buyer { secret: 100 };
+        assert_eq!(buyer.secret_numbers().last(), Some(15273692));
+
+        let buyer = Buyer { secret: 2024 };
+        assert_eq!(buyer.secret_numbers().last(), Some(8667524));
     }
 
     #[test]
